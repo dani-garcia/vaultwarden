3e1afb139c80f32f2b187af4ca8ea04aa86cb977
Daniel García <dani-garcia@users.noreply.github.com>
2018-09-12 23:58:02 +0200

    Remove unnecessary return

--- src/api/core/accounts.rs
+++ src/api/core/accounts.rs
@@ -288,7 +288,7 @@ fn password_hint(data: JsonUpcase<PasswordHintData>, conn: DbConn) -> EmptyResul
     let data: PasswordHintData = data.into_inner().data;
 
     if !is_valid_email(&data.Email) {
-        return err!("This email address is not valid...");
+        err!("This email address is not valid...");
     }
 
     let user = User::find_by_mail(&data.Email, &conn);
