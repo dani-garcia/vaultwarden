c36f49e3c697579fe1035fe2e27176151a01bd37
unknown <etienneorlhac@gmail.com>
2019-10-30 01:24:22 +0100

    use uid

--- src/api/identity.rs
+++ src/api/identity.rs
@@ -96,7 +96,7 @@ fn _password_login(data: ConnectData, conn: DbConn, ip: ClientIp) -> JsonResult 
         // Attempt to bind to ldap with these credentials
         match LdapConn::new(CONFIG.ldap_host().as_str()) {
             Ok(ldap) => {
-                let bind_dn = format!("sAMAccountName={},{}", ldap_username, CONFIG.ldap_search_base_dn());
+                let bind_dn = format!("uid={},{}", ldap_username, CONFIG.ldap_search_base_dn());
                 let bind = ldap.simple_bind(bind_dn.as_ref(), password)?.success();
 
                 if bind.is_err() {
