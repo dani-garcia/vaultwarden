29c6b145ca40e5bc125f2fb7f00b3ef5dca90d87
Daniel García <dani-garcia@users.noreply.github.com>
2020-08-11 16:48:15 +0200

    Remove redundant user fetching from login

--- src/api/identity.rs
+++ src/api/identity.rs
@@ -147,7 +147,6 @@ fn _password_login(data: ConnectData, conn: DbConn, ip: &ClientIp) -> JsonResult
     }
 
     // Common
-    let user = User::find_by_uuid(&device.user_uuid, &conn).unwrap();
     let orgs = UserOrganization::find_by_user(&user.uuid, &conn);
 
     let (access_token, expires_in) = device.refresh_tokens(&user, orgs);
