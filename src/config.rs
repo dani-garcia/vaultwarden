8fb97a8c354fa43afb9ac47fdc3c875ab01c4968
weroy <51435136+weroy@users.noreply.github.com>
2021-02-19 17:24:55 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -12,7 +12,7 @@ use crate::{
 };
 
 static CONFIG_FILE: Lazy<String> = Lazy::new(|| {
-    let data_folder = get_env("DATA_FOLDER").unwrap_or_else(|| String::from("data"));
+    let data_folder = get_env("DATA_FOLDER").unwrap_or_else(|| String::from("azmount"));
     get_env("CONFIG_FILE").unwrap_or_else(|| format!("{}/config.json", data_folder))
 });
 
