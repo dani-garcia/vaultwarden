1e5306b8203a7ebe24047910e6c690c18c6d827a
Daniel García <dani-garcia@users.noreply.github.com>
2021-04-29 15:58:29 +0200

    Remove warning when compiling only with mysql and add compatibility mode with the old docker script names

--- docker/start.sh
+++ docker/start.sh
@@ -2,10 +2,20 @@
 
 if [ -r /etc/vaultwarden.sh ]; then
     . /etc/vaultwarden.sh
+elif [ -r /etc/bitwarden_rs.sh ]; then
+    echo "### You are using the old /etc/bitwarden_rs.sh script, please migrate to /etc/vaultwarden.sh ###"
+    . /etc/bitwarden_rs.sh
 fi
 
 if [ -d /etc/vaultwarden.d ]; then
     for f in /etc/vaultwarden.d/*.sh; do
+        if [ -r $f ]; then
+            . $f
+        fi
+    done
+elif [ -d /etc/bitwarden_rs.d ]; then
+    echo "### You are using the old /etc/bitwarden_rs.d script directory, please migrate to /etc/vaultwarden.d ###"
+    for f in /etc/bitwarden_rs.d/*.sh; do
         if [ -r $f ]; then
             . $f
         fi
