c49ee47de056cad058bcf2dc2b8c19d621bc6a39
Jeremy Lin <jeremy.lin@gmail.com>
2021-10-31 17:50:00 -0700

    Fix PostgreSQL migration
    The PostgreSQL migration should have used `TIMESTAMP` rather than `DATETIME`.

--- migrations/postgresql/2021-10-24-164321_add_2fa_incomplete/up.sql
+++ migrations/postgresql/2021-10-24-164321_add_2fa_incomplete/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE twofactor_incomplete (
   user_uuid   VARCHAR(40) NOT NULL REFERENCES users(uuid),
   device_uuid VARCHAR(40) NOT NULL,
   device_name TEXT        NOT NULL,
-  login_time  DATETIME    NOT NULL,
+  login_time  TIMESTAMP   NOT NULL,
   ip_address  TEXT        NOT NULL,
 
   PRIMARY KEY (user_uuid, device_uuid)
