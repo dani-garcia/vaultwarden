64edc49392f1786aebf6a5a0c9b60742ca734d6e
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-02-06 23:19:08 +0100

    change description of domain configuration
    Vaultwarden send won't work if the domain includes a trailing slash.
    This should be documented, as it may lead to confusion amoung users.

--- src/config.rs
+++ src/config.rs
@@ -401,7 +401,8 @@ make_config! {
     /// General settings
     settings {
         /// Domain URL |> This needs to be set to the URL used to access the server, including 'http[s]://'
-        /// and port, if it's different than the default. Some server functions don't work correctly without this value
+        /// and port, if it's different than the default, but excluding a trailing slash.
+        /// Some server functions don't work correctly without this value
         domain:                 String, true,   def,    "http://localhost".to_string();
         /// Domain Set |> Indicates if the domain is set by the admin. Otherwise the default will be used.
         domain_set:             bool,   false,  def,    false;
