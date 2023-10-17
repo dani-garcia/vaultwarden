30b408eaa9f3d84c4aee849597ba1f25d4ba06ad
Mathieu Valois <mvalois@teicee.com>
2023-10-17 11:18:39 +0200

    LastActive is null instead of 'Never' when never connected

--- src/api/admin.rs
+++ src/api/admin.rs
@@ -328,7 +328,7 @@ async fn get_users_json(_token: AdminToken, mut conn: DbConn) -> Json<Value> {
         usr["CreatedAt"] = json!(format_naive_datetime_local(&u.created_at, DT_FMT));
         usr["LastActive"] = match u.last_active(&mut conn).await {
             Some(dt) => json!(format_naive_datetime_local(&dt, DT_FMT)),
-            None => json!("Never"),
+            None => json!(None::<String>),
         };
         users_json.push(usr);
     }
