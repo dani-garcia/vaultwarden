f1c4f26402b4ac6e3c2cac71cb4e8a243be6b6b9
dfunkt <daniel.barabasa@gmail.com>
2024-08-07 11:31:52 +0300

    Remove lowercase conversion for featureStates
    - needed to match Bitwarden, some of the feature flags might have uppercase characters (for example: ```PM-4154-bulk-encryption-service```)

--- src/util.rs
+++ src/util.rs
@@ -730,8 +730,7 @@ pub fn convert_json_key_lcase_first(src_json: Value) -> Value {
 
 /// Parses the experimental client feature flags string into a HashMap.
 pub fn parse_experimental_client_feature_flags(experimental_client_feature_flags: &str) -> HashMap<String, bool> {
-    let feature_states =
-        experimental_client_feature_flags.to_lowercase().split(',').map(|f| (f.trim().to_owned(), true)).collect();
+    let feature_states = experimental_client_feature_flags.split(',').map(|f| (f.trim().to_owned(), true)).collect();
 
     feature_states
 }
