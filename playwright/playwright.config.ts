cb811d958599831632fac3b098a08412d840036c
Timshel <Timshel@users.noreply.github.com>
2024-09-05 19:22:37 +0200

    Add 2fa authenticator test

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -30,6 +30,8 @@ export default defineConfig({
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: process.env.DOMAIN,
         browserName: 'firefox',
+        locale: 'en-GB',
+        timezoneId: 'Europe/London',
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
     },
