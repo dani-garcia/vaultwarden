de01111082cea605ad1788e9596b0078ef4fcc73
Mathijs van Veluw <black.dex@gmail.com>
2024-09-20 12:06:06 +0200

    Fix Device Type column for 2FA migration (#4971)

--- migrations/sqlite/2024-09-04-091351_use_device_type_for_mails/up.sql
+++ migrations/sqlite/2024-09-04-091351_use_device_type_for_mails/up.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `twofactor_incomplete` ADD COLUMN `device_type` INTEGER NOT NULL;
+ALTER TABLE twofactor_incomplete ADD COLUMN device_type INTEGER NOT NULL DEFAULT 14; -- 14 = Unknown Browser
