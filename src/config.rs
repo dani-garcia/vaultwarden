3f5a8d6eef8af295efcbed7cb0861f382143126a
Ben Sherman <bennettmsherman@gmail.com>
2025-03-15 10:40:59 -0700

    Add support for mutual-tls feature flag

--- src/config.rs
+++ src/config.rs
@@ -842,6 +842,7 @@ fn validate_config(cfg: &ConfigItems) -> Result<(), Error> {
         "inline-menu-positioning-improvements",
         "ssh-key-vault-item",
         "ssh-agent",
+        "mutual-tls"
     ];
     let configured_flags = parse_experimental_client_feature_flags(&cfg.experimental_client_feature_flags);
     let invalid_flags: Vec<_> = configured_flags.keys().filter(|flag| !KNOWN_FLAGS.contains(&flag.as_str())).collect();
