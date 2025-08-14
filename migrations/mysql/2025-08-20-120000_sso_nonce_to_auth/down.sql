4ceb6abe04a4d559952c93415a741ba8e8fffc99
Timshel <timshel@users.noreply.github.com>
2025-08-14 13:47:57 +0200

    Improve sso auth flow

---
+++ migrations/mysql/2025-08-20-120000_sso_nonce_to_auth/down.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS sso_auth;
+
+CREATE TABLE sso_nonce (
+    state               VARCHAR(512) NOT NULL PRIMARY KEY,
+    nonce               TEXT NOT NULL,
+    verifier            TEXT,
+    redirect_uri        TEXT NOT NULL,
+    created_at          TIMESTAMP NOT NULL DEFAULT now()
+);
