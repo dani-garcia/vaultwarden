cb5095b54df368e3658f7a485a3b86b19449ab15
Timshel <timshel@480s>
2025-12-01 16:41:51 +0100

    Org.put_policy type not in body anymore

--- src/api/core/organizations.rs
+++ src/api/core/organizations.rs
@@ -2057,8 +2057,6 @@ async fn get_policy(org_id: OrganizationId, pol_type: i32, headers: AdminHeaders
 #[derive(Deserialize)]
 struct PolicyData {
     enabled: bool,
-    #[serde(rename = "type")]
-    _type: i32,
     data: Option<Value>,
 }
 
