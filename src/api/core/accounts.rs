2d91a9460bcbd8fdc1a077dd647885f2711710ad
Timshel <github@mailtm.org>
2025-12-06 22:14:20 +0100

    Fix admin invite with SSO (#6498)
    Co-authored-by: Timshel <timshel@users.noreply.github.com>

--- src/api/core/accounts.rs
+++ src/api/core/accounts.rs
@@ -378,7 +378,7 @@ async fn post_set_password(data: Json<SetPasswordData>, headers: Headers, conn: 
     }
 
     if let Some(identifier) = data.org_identifier {
-        if identifier != crate::sso::FAKE_IDENTIFIER {
+        if identifier != crate::sso::FAKE_IDENTIFIER && identifier != crate::api::admin::FAKE_ADMIN_UUID {
             let org = match Organization::find_by_uuid(&identifier.into(), &conn).await {
                 None => err!("Failed to retrieve the associated organization"),
                 Some(org) => org,
