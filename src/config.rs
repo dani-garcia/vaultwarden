ba40076bcb3e0720412369c2f55b7703d74b603c
Matt Aaron <13080357+matt-aaron@users.noreply.github.com>
2026-03-15 13:43:25 -0400

    Enable flag directly and remove stale flags

--- src/config.rs
+++ src/config.rs
@@ -1052,8 +1052,6 @@ fn validate_config(cfg: &ConfigItems) -> Result<(), Error> {
         "cxp-export-mobile",
         // Webauthn Related Origins
         "pm-30529-webauthn-related-origins",
-        // Innovation Team
-        "pm-19148-innovation-archive",
     ];
     let configured_flags = parse_experimental_client_feature_flags(&cfg.experimental_client_feature_flags);
     let invalid_flags: Vec<_> = configured_flags.keys().filter(|flag| !KNOWN_FLAGS.contains(&flag.as_str())).collect();
