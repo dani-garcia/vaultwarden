947d3b7b84d9ca0ba2a41e112364784e449de9ab
Berk D. Demir <bdd@mindcast.org>
2026-07-24 16:14:21 -0700

    Add `pm-32009-new-item-types` feature flag
    Introduced in 2026.4.0 for all clients, enables new item types bank
    account, driver's license, and passport.

--- src/config.rs
+++ src/config.rs
@@ -1436,6 +1436,8 @@ pub const SUPPORTED_FEATURE_FLAGS: &[&str] = &[
     "cxp-export-mobile",
     // Platform Team
     "pm-30529-webauthn-related-origins",
+    // Vault Team
+    "pm-32009-new-item-types",
 ];
 
 impl Config {
