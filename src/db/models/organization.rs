38630bedacbca38aefcc00bd7544fb797bbfafaa
Alexander Neff <alex99.neff@gmx.de>
2026-08-05 10:24:31 +0200

    Revert PR #6890

--- src/db/models/organization.rs
+++ src/db/models/organization.rs
@@ -522,8 +522,7 @@ impl Membership {
             "familySponsorshipValidUntil": null,
             "familySponsorshipToDelete": null,
             "accessSecretsManager": false,
-            // limit collection creation to managers with access_all permission to prevent issues
-            "limitCollectionCreation": self.atype < MembershipType::Manager || !self.access_all,
+            "limitCollectionCreation": self.atype < MembershipType::Manager, // If less then a manager return true, to limit collection creations
             "limitCollectionDeletion": true,
             "limitItemDeletion": false,
             "allowAdminAccessToAllCollectionItems": true,
