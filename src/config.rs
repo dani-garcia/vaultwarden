d76c65921520b598a99ad5fe536cc796928304bf
tom27052006 <83423411+tom27052006@users.noreply.github.com>
2026-09-16 17:15:09 +0000

    Add basic auth response client feature flag

--- src/config.rs
+++ src/config.rs
@@ -1427,6 +1427,7 @@ pub const SUPPORTED_FEATURE_FLAGS: &[&str] = &[
     "pm-5594-safari-account-switching",
     "pm-32413-multi-client-password-management",
     // Autofill Team
+    "enable-basic-auth-response",
     "ssh-agent",
     "ssh-agent-v2",
     // Key Management Team
