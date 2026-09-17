613bd8217dafdb79e27bdcf76524b6a6ea8bb8d2
tom27052006 <83423411+tom27052006@users.noreply.github.com>
2026-09-17 21:25:18 +0200

    Support the vault banner policy

--- src/db/models/org_policy.rs
+++ src/db/models/org_policy.rs
@@ -49,6 +49,7 @@ pub enum OrgPolicyType {
     // AutotypeDefaultSetting = 17, // Not supported yet
     // AutoConfirm = 18, // Not supported (not implemented yet)
     // BlockClaimedDomainAccountCreation = 19, // Not supported (Not AGPLv3 Licensed)
+    OrganizationUserNotification = 20,
 }
 
 // https://github.com/bitwarden/server/blob/9ebe16587175b1c0e9208f84397bb75d0d595510/src/Core/AdminConsole/Models/Data/Organizations/Policies/SendOptionsPolicyData.cs#L5
